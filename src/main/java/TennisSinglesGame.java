3555123dcc46f7a31088e7862171f1e98ab2135f
vildozo <vildozo@outlook.com>
2016-02-14 18:53:46 -0400

    eliminar funciones no necesarias y renombrar

--- src/main/java/TennisSinglesGame.java
+++ src/main/java/TennisSinglesGame.java
@@ -1,5 +1,5 @@
 
-public class TennisGame2 implements TennisGame
+public class TennisSinglesGame implements TennisGame
 {
     public int pointPlayer1 = 0;
     public int pointPlayer2 = 0;
@@ -9,39 +9,12 @@ public class TennisGame2 implements TennisGame
     private String namePlayer1;
     private String namePlayer2;
 
-    public TennisGame2(String player1Name, String player2Name) {
+    public TennisSinglesGame(String player1Name, String player2Name) {
         this.namePlayer1 = player1Name;
         this.namePlayer2 = player2Name;
     }
     
-    public String tiedGame(){
-        String score = "";
-        if (pointPlayer1 == pointPlayer2){
-            if (isLove(pointPlayer1))
-                score = "Love";
-            if (isFifteen(pointPlayer1))
-                score = "Fifteen";
-            if (isThirty(pointPlayer1))
-                score = "Thirty";
-            score += "-All";
-            if ( pointPlayer1>=3)
-                    score = "Deuce";
-        }
-        return score;
-    }
-    
-    public String ConvertPointToLiteral(int point){
-        String literal = "";
-            if (point==0)
-                literal="Love";
-            if (point==1)
-                literal = "Fifteen";
-            if (point==2)
-                literal = "Thirty";
-            if (point==3)
-                literal = "Forty";
-        return literal;
-    }
+ 
 
     public String getScore(){
         String score = "";
@@ -111,36 +84,48 @@ public class TennisGame2 implements TennisGame
         return false;
     }
     
-    public void SetP1Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P1Score();
-        }
-            
-    }
     
-    public void SetP2Score(int number){
-        
-        for (int i = 0; i < number; i++)
-        {
-            P2Score();
-        }
-            
-    }
-    
-    public void P1Score(){
+    public void Player1Scores(){
         pointPlayer1++;
     }
     
-    public void P2Score(){
+    public void Player2Scores(){
         pointPlayer2++;
     }
 
     public void wonPoint(String player) {
         if (player == "player1")
-            P1Score();
+            Player1Scores();
         else
-            P2Score();
+            Player2Scores();
+    }
+    
+       public String tiedGame(){
+        String score = "";
+        if (pointPlayer1 == pointPlayer2){
+            if (isLove(pointPlayer1))
+                score = "Love";
+            if (isFifteen(pointPlayer1))
+                score = "Fifteen";
+            if (isThirty(pointPlayer1))
+                score = "Thirty";
+            score += "-All";
+            if ( pointPlayer1>=3)
+                    score = "Deuce";
+        }
+        return score;
+    }
+    
+    public String ConvertPointToLiteral(int point){
+        String literal = "";
+            if (point==0)
+                literal="Love";
+            if (point==1)
+                literal = "Fifteen";
+            if (point==2)
+                literal = "Thirty";
+            if (point==3)
+                literal = "Forty";
+        return literal;
     }
 }
--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -79,7 +79,7 @@ public class TennisTest {
 
     @Test
     public void checkAllScoresTennisGame2() {
-        TennisGame2 game = new TennisGame2("player1", "player2");
+        TennisSinglesGame game = new TennisSinglesGame("player1", "player2");
         checkAllScores(game);
     }
 }
